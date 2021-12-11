41441c560f2c6525e606f5fdbee4d4db3fdc2a9a
Orçun Tuna <orcun.tuna@outlook.com>
2021-12-11 10:36:40 +0300

    feat: create login page with simple ui

---
+++ pages/login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <h1 class="mt-10 text-center text-4xl font-extrabold text-indigo-700">
+        ProdTest
+      </h1>
+      <h2 class="mt-4 text-center text-3xl font-semibold text-gray-800">
+        {{$t('Sign in to your account')}}
+      </h2>
+    </div>
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <form class="space-y-6" action="#" method="POST">
+          <div>
+            <label for="email" class="block text-sm font-medium text-gray-700">
+              {{$t('Email address')}}
+            </label>
+            <div class="mt-1">
+              <input id="email" name="email" type="email" autocomplete="email" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            </div>
+          </div>
+
+          <div>
+            <label for="password" class="block text-sm font-medium text-gray-700">
+              {{$t('Password')}}
+            </label>
+            <div class="mt-1">
+              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              {{$t('Sign in')}}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    htmlAttrs: {
+      class: 'h-full bg-gray-50'
+    },
+    bodyAttrs: {
+      class: 'h-full'
+    },
+  }
+}
+</script>
