bfdf49e5e31a4450f5fc37a775cd170211b2d12f
Orçun Tuna <orcun.tuna@outlook.com>
2021-12-11 19:50:39 +0300

    feat: add 404 not found page

---
+++ pages/404.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+    <div class="max-w-max mx-auto">
+      <main class="sm:flex">
+        <p class="text-4xl font-extrabold text-indigo-600 sm:text-5xl">404</p>
+        <div class="sm:ml-6">
+          <div class="sm:border-l sm:border-gray-200 sm:pl-6">
+            <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">{{$t('Page not found')}}</h1>
+            <p class="mt-1 text-base text-gray-500">{{$t('Please check the URL in the address bar and try again')}}</p>
+          </div>
+          <div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+            <NuxtLink to='/projects' class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              {{$t('Go back projects')}}
+            </NuxtLink>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page404',
+  head: {
+    htmlAttrs: {
+      class: 'h-full'
+    },
+    bodyAttrs: {
+      class: 'h-full'
+    },
+  },
+}
+</script>
