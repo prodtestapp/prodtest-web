72f0dc6173b50dd0bf31d851d02de767be50ce45
Ahmet Korkmaz <muratahmetkorkmaz@hotmail.com>
2021-12-12 13:57:26 +0300

    Add variable to language file

---
+++ pages/account.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <update-account class='mb-5' />
+    <change-password />
+  </div>
+</template>
+
+<script>
+import UpdateAccount from '~/components/UpdateAccount';
+import ChangePassword from '~/components/ChangePassword';
+
+export default {
+  name: 'Account',
+  components: { UpdateAccount, ChangePassword },
+  layout: 'Dashboard',
+  middleware: 'auth',
+}
+</script>
+
+<style scoped>
+
+</style>
