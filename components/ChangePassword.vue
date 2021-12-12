723ac2a7b9a2e97aa40bfc8b0cb6e93a4e93980c
Ahmet Korkmaz <muratahmetkorkmaz@hotmail.com>
2021-12-12 14:08:23 +0300

    Fix change password input style

--- components/ChangePassword.vue
+++ components/ChangePassword.vue
@@ -5,16 +5,16 @@
         <div class='grid grid-cols-3 gap-6'>
           <div class='col-span-3 sm:col-span-2'>
             <label for='password' class='block text-sm font-medium text-gray-700'>{{ $t('Password') }}</label>
-            <input id='password' v-model='password' type='password' name='password' autocomplete='given-name'
-                   class='mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
+            <input id='password' v-model='password' :class='{"border-red-300": validationErrors.password}' name='email'
+                   type='password' required=''
+                   class='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' />
 
             <label for='password-confirmation' class='block text-sm font-medium text-gray-700'>
               {{ $t('Password Again') }}
             </label>
-            <input id='password-confirmation' v-model='password_confirmation' type='password' name='password-confirmation'
-                   autocomplete='given-name'
-                   class='mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
-
+            <input id='password-confirmation' v-model='password_confirmation' :class='{"border-red-300": validationErrors.password_confirmation}' name='email'
+                   type='password' required=''
+                   class='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' />
           </div>
         </div>
       </div>
@@ -34,7 +34,8 @@ export default {
   data() {
     return {
       password: '',
-      password_confirmation: ''
+      password_confirmation: '',
+      validationErrors: {},
     }
   },
   methods: {
@@ -42,8 +43,8 @@ export default {
       this.$axios
         .post('auth/change-password', { password: this.password, password_confirmation: this.password_confirmation })
         .then(() => {
-          this.password = '';
-          this.password_confirmation = '';
+          this.password = ''
+          this.password_confirmation = ''
         })
         .catch(error => {
           this.$toast.error(error.response.data.message)
