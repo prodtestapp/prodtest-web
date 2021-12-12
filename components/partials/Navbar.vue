f9dccf2d12032ff6eb9ed5a49fae4498ced1c38f
Ahmet Korkmaz <muratahmetkorkmaz@hotmail.com>
2021-12-12 14:14:49 +0300

    Fix styling

--- components/partials/Navbar.vue
+++ components/partials/Navbar.vue
@@ -18,7 +18,7 @@
         </div>
         <div class="hidden sm:ml-6 sm:flex sm:items-center">
           <p class="text-gray-400 inline-flex items-center px-1 pt-1 mr-4 text-sm font-medium" aria-current="page">
-            {{$t('Hello, ')}} {{$auth.user.full_name}}
+            {{$t('Hello, ')}} {{$auth.user.data.full_name}}
           </p>
           <NuxtLink to='/account' class="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 mr-3 text-sm font-medium" aria-current="page">
             Settings
--- pages/account.vue
+++ pages/account.vue
@@ -1,22 +1,18 @@
 <template>
-  <div>
+  <div class='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2'>
     <update-account class='mb-5' />
     <change-password />
   </div>
 </template>
 
 <script>
-import UpdateAccount from '~/components/UpdateAccount';
-import ChangePassword from '~/components/ChangePassword';
+import UpdateAccount from '~/components/UpdateAccount'
+import ChangePassword from '~/components/ChangePassword'
 
 export default {
   name: 'Account',
   components: { UpdateAccount, ChangePassword },
   layout: 'Dashboard',
-  middleware: 'auth',
+  middleware: 'auth'
 }
 </script>
-
-<style scoped>
-
-</style>
