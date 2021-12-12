c3d82c0cef981c3545491e18f78ae42205e1f91d
Ahmet Korkmaz <muratahmetkorkmaz@hotmail.com>
2021-12-12 14:54:54 +0300

    Fix style

--- components/ChangePassword.vue
+++ components/ChangePassword.vue
@@ -9,7 +9,7 @@
                    type='password' required=''
                    class='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm' />
 
-            <label for='password-confirmation' class='block text-sm font-medium text-gray-700 mt-2'>
+            <label for='password-confirmation' class='block text-sm font-medium text-gray-700 mt-4'>
               {{ $t('Password Again') }}
             </label>
             <input id='password-confirmation' v-model='password_confirmation' :class='{"border-red-300": validationErrors.password_confirmation}' name='email'
