cf8130277c86851cd344250e51b7347acd8db566
Orçun Tuna <orcun.tuna@outlook.com>
2022-01-15 13:50:09 +0300

    update table for expandable custom row data

--- pages/cases/_id.vue
+++ pages/cases/_id.vue
@@ -23,33 +23,9 @@
       <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table class="min-w-full divide-y divide-gray-200">
-              <thead class="bg-gray-50">
-              <tr>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  #
-                </th><th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  {{$t('Name')}}
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  {{$t('Key')}}
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  {{$t('Method')}}
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  {{$t('URL')}}
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  {{$t('Status')}}
-                </th>
-                <th scope="col" class="relative px-6 py-3">
-                  <span class="sr-only">{{ $t("Actions") }}</span>
-                </th>
-              </tr>
-              </thead>
-              <draggable v-model="steps" tag="tbody" class="bg-white divide-y divide-gray-200" handle=".handle" @change='onChangeStepsOrder'>
-                <tr v-for='step in steps' :key='`step-${step.id}`'>
+            <draggable v-model="steps" tag="table" class="min-w-full divide-y divide-gray-200" handle=".handle" @change='onChangeStepsOrder'>
+              <tbody v-for='step in steps' :key='`step-${step.id}`' class="bg-white divide-y divide-gray-200">
+                <tr>
                   <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                     <svg class="h-5 w-5 handle" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -100,11 +76,40 @@
                 </span>
                   </td>
                 </tr>
+                <tr v-if='expandedStepId === step.id'>
+                  <td colspan='7'>
+                    <div class='my-6 flex justify-center'>data</div>
+                  </td>
+                </tr>
                 <tr v-if='steps.length === 0'>
                   <td class='text-sm text-gray-800 text-center py-10' colspan='7'>{{$t('This table is empty yet')}}</td>
                 </tr>
-              </draggable>
-            </table>
+              </tbody>
+              <thead slot='header' class="bg-gray-50">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  #
+                </th><th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                {{$t('Name')}}
+              </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  {{$t('Key')}}
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  {{$t('Method')}}
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  {{$t('URL')}}
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  {{$t('Status')}}
+                </th>
+                <th scope="col" class="relative px-6 py-3">
+                  <span class="sr-only">{{ $t("Actions") }}</span>
+                </th>
+              </tr>
+              </thead>
+            </draggable>
           </div>
         </div>
       </div>
